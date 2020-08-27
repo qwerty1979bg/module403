9bd8970bfc0ea9774b0e4575311af78bad365e1c
Filip Dimitrov <github@filip-dimitrov.eu>
2020-08-27 22:04:01 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@
 #  source = "git@github.com:terraform-aws-modules/terraform-aws-security-group.git"
 #}
 
-module "security-group-direct-https" {
-  source = "https://github.com/terraform-aws-modules/terraform-aws-security-group/archive/v3.15.0.zip"
+#module "security-group-direct-https" {
+#  source = "https://github.com/terraform-aws-modules/terraform-aws-security-group/archive/v3.15.0.zip"
+#}
+
+module "security-group-direct-git" {
+  source = "git::https://github.com/terraform-aws-modules/terraform-aws-security-group.git"
 }

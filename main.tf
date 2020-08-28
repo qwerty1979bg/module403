1090e53beadfc9928027bd200c47f086c9f54bbe
Filip Dimitrov <github@filip-dimitrov.eu>
2020-08-28 06:05:58 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
-#module "security-group-fail-registry" {
-#  source  = "terraform-aws-modules/security-group/aws"
-#}
+module "security-group-fail-registry" {
+  source  = "terraform-aws-modules/security-group/aws"
+}
   
 #module "security-group-direct-ssh" {
 #  source = "git@github.com:terraform-aws-modules/terraform-aws-security-group.git"
